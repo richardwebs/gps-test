8bd7d8aafba5d0b778e2d12313bc90920882d367
Richard Webster <richardwebs@gmail.com>
2021-11-02 18:21:57 +0000

    integration tests
    integration tests

---
+++ gps.codingtest.tests/Services/EmailServiceIntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using gps.codingtest.core.ServiceInterfaces;
+using gps.codingtest.core.Services;
+using Xunit;
+
+namespace gps.codingtest.tests.Services
+{
+    [CollectionDefinition("")]
+    public class EmailServiceIntegrationTests
+    {
+        private readonly IEmailService _service;
+
+        public EmailServiceIntegrationTests()
+        {
+            _service = new EmailService();
+        }
+
+        [Fact]
+        public void Send_Success()
+        {
+            var result = _service.Send("to", "from", "subject", "message");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsServiceRunning_Success()
+        {
+            var result = _service.IsServiceRunning();
+            Assert.True(result);
+        }
+
+    }
+}
---
+++ gps.codingtest.tests/Services/SmsServiceIntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using gps.codingtest.core.ServiceInterfaces;
+using gps.codingtest.core.Services;
+using Xunit;
+
+namespace gps.codingtest.tests.Services
+{
+    [CollectionDefinition("")]
+    public class SmsServiceIntegrationTests
+    {
+        private readonly ISmsService _service;
+
+        public SmsServiceIntegrationTests()
+        {
+            _service = new SmsService();
+        }
+
+        [Fact]
+        public void Send_Success()
+        {
+            var result = _service.Send("to", "from", "message");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsServiceRunning_Success()
+        {
+            var result = _service.IsServiceRunning();
+            Assert.True(result);
+        }
+
+    }
+}
